b767d4d0f6b361bb831c8ad296d81f1f1599bc62
Nico <Nico@wlan10-9-45-7.hiz-saarland.de>
2018-05-15 18:10:26 +0200

    s

--- Uebung17/src/ApplyFakTopLevel.java
+++ Uebung17/src/ApplyFakTopLevel.java
@@ -10,15 +10,37 @@
 public class ApplyFakTopLevel implements MyFunction {
 
     /**
-     * Überschreibt die Methode apply des Interfaces MyFunction 
-     * mit der Funktion: f(x) = x!
+     * Überschreibt die Methode apply des Interfaces MyFunction mit der Funktion:
+     * f(x) = x!
      * 
-     * @param Zahl	die Fakultät, die berechnet werden soll
-     * @return Zahl	das Ergebnis von i!
+     * @param Zahl
+     *            die Fakultät, die berechnet werden soll
+     * @return Zahl das Ergebnis von i!
      */
     @Override
     public int apply(int i) {
 	return MyMathFunctions.fak(i);
+    }
+
+    /*
+     * Innere Klasse: ApplyFakStaticNested, implementiert das Interface MyFunction
+     */
+    public static class ApplyFakStaticNested implements MyFunction {
+
+	/**
+	 * Überschreibt die Methode apply des Interfaces MyFunction mit der Funktion:
+	 * f(x) = x!
+	 * 
+	 * @param Zahl
+	 *            die Fakultät, die berechnet werden soll
+	 * @return Zahl das Ergebnis von i!
+	 */
+	@Override
+	public int apply(int i) {
+	    // TODO Auto-generated method stub
+	    return 0;
+	}
+
     }
 
 }
